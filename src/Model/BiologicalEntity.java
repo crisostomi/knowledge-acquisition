d247fc3f7914deed2c48280af44443c4dd9ae4ae
crisostomi <donatocrisostomi@gmail.com>
2019-08-14 11:06:12 +0200

    add LinkTypeAdditionalKnowledge

--- src/Model/BiologicalEntity.java
+++ src/Model/BiologicalEntity.java
@@ -1,17 +1,28 @@
 package Model;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public abstract class BiologicalEntity {
     private final String id; //unique
     private String name;
+    private Set<LinkTypeAdditionalKnowledge> additionalKnowledge;
 
     public BiologicalEntity(String id, String name) {
         this.id = id;
         this.name = name;
     }
 
+    public void insertLinkAdditionalKnowledge(AdditionalKnowledgeType addKnowType, String value){
+        LinkTypeAdditionalKnowledge link = new LinkTypeAdditionalKnowledge(this, addKnowType, value);
+        this.additionalKnowledge.add(link);
+    }
+
     public abstract void override();
 
-    public Map<AdditionalKnowledgeType, String> getAdditionalKnowledge(){
-
-    }
+//    public Map<AdditionalKnowledgeType, String> getAdditionalKnowledge(){
+//        Map<AdditionalKnowledgeType, String> addKnow = new HashMap<AdditionalKnowledgeType, String>();
+//
+//    }
 }
---
+++ src/Model/LinkTypeAdditionalKnowledge.java
@@ -0,0 +1,41 @@
+package Model;
+
+import java.util.Objects;
+
+public class LinkTypeAdditionalKnowledge {
+    private BiologicalEntity biologicalEntity;
+    private AdditionalKnowledgeType addKnowType;
+    private String value;
+
+    public LinkTypeAdditionalKnowledge(BiologicalEntity biologicalEntity, AdditionalKnowledgeType addKnowType, String value) {
+        this.biologicalEntity = biologicalEntity;
+        this.addKnowType = addKnowType;
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LinkTypeAdditionalKnowledge that = (LinkTypeAdditionalKnowledge) o;
+        return biologicalEntity == that.biologicalEntity &&
+                addKnowType == that.addKnowType;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(biologicalEntity, addKnowType);
+    }
+
+    public BiologicalEntity getBiologicalEntity() {
+        return biologicalEntity;
+    }
+
+    public AdditionalKnowledgeType getAddKnowType() {
+        return addKnowType;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
