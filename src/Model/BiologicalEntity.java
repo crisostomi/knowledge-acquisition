844def502d4594db2bacc0218e1c7a2476bf32b5
crisostomi <donatocrisostomi@gmail.com>
2019-08-14 10:18:35 +0200

    add BiologicalEntity

--- src/Model/BiologicalEntity.java
+++ src/Model/BiologicalEntity.java
@@ -1,11 +1,17 @@
 package Model;
 
-public class BiologicalEntity {
+public abstract class BiologicalEntity {
     private final String id; //unique
     private String name;
 
     public BiologicalEntity(String id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public abstract void override();
+
+    public Map<AdditionalKnowledgeType, String> getAdditionalKnowledge(){
+
     }
 }
