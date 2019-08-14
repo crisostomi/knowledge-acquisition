014496277e60da98d285f858fd07941375470fa1
crisostomi <donatocrisostomi@gmail.com>
2019-08-14 10:13:49 +0200

    add Model

---
+++ src/Model/BiologicalEntity.java
@@ -0,0 +1,11 @@
+package Model;
+
+public class BiologicalEntity {
+    private final String id; //unique
+    private String name;
+
+    public BiologicalEntity(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
--- src/Model/Model.java
+++ src/Model/Model.java
@@ -2,7 +2,16 @@ package Model;
 
 public class Model {
 
-    public Model(){
+    public void overrideModel(Model other){
 
     }
+
+    public BiologicalEntity getBioEntityById(String id){
+        return null;
+    }
+
+    public void overrideEntity(BiologicalEntity other){
+
+    }
+
 }
