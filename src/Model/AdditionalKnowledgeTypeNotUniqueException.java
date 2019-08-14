c3b8226a07b20a0116eddc97798ad78a3f7e3204
scacio <andrea.caciolai@gmail.com>
2019-08-14 11:56:24 +0200

    add AdditionalKnowledgeType

---
+++ src/Model/AdditionalKnowledgeTypeNotUniqueException.java
@@ -0,0 +1,6 @@
+package Model;
+
+public class AdditionalKnowledgeTypeNotUniqueException extends Exception {
+    AdditionalKnowledgeTypeNotUniqueException() {super();}
+    AdditionalKnowledgeTypeNotUniqueException(String message) {super(message);}
+}
