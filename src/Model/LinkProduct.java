ba33a41fab28a0ef2eb598a1a50fcc9ef1bd639d
crisostomi <donatocrisostomi@gmail.com>
2019-08-20 16:27:11 +0200

    add LinkProduct and LinkReactant

--- src/Model/LinkProduct.java
+++ src/Model/LinkProduct.java
@@ -1,4 +1,23 @@
 package Model;
 
+import DataTypes.PreconditionsException;
+
 public class LinkProduct {
+
+    public static void insertLink(Species species, Reaction reaction, int stoichiometry) throws PreconditionsException {
+        // Creo un pass: solo i metodi di questa classe possono farlo!
+        LinkProduct pass = new LinkProduct();
+        // Creo il link
+        LinkTypeProduct link = new LinkTypeProduct(species, reaction, stoichiometry);
+        // Inserisco il link nei due oggetti esibendo il pass
+        species.insertLinkProduct(pass, link);
+        reaction.insertLinkProduct(pass, link);
+    }
+
+    public static void removeLink(LinkTypeProduct link)
+            throws PreconditionsException {
+        LinkProduct pass = new LinkProduct();
+        link.getSpecies().removeLinkProduct(pass, link);
+        link.getReaction().removeLinkProduct(pass, link);
+    }
 }
--- src/Model/LinkReactant.java
+++ src/Model/LinkReactant.java
@@ -1,4 +1,23 @@
 package Model;
 
+import DataTypes.PreconditionsException;
+
 public class LinkReactant {
+
+    public static void insertLink(Species species, Reaction reaction, int stoichiometry) throws PreconditionsException {
+        // Creo un pass: solo i metodi di questa classe possono farlo!
+        LinkReactant pass = new LinkReactant();
+        // Creo il link
+        LinkTypeReactant link = new LinkTypeReactant(species, reaction, stoichiometry);
+        // Inserisco il link nei due oggetti esibendo il pass
+        species.insertLinkReactant(pass, link);
+        reaction.insertLinkReactant(pass, link);
+    }
+
+    public static void removeLink(LinkTypeReactant link)
+            throws PreconditionsException {
+        LinkReactant pass = new LinkReactant();
+        link.getSpecies().removeLinkReactant(pass, link);
+        link.getReaction().removeLinkReactant(pass, link);
+    }
 }
