4018d8456021e25282d40e4254b176fabccb959d
scacio <andrea.caciolai@gmail.com>
2019-09-13 11:25:10 +0200

    add catalogue of pathways

--- src/main/java/Util/PathwayFinder.java
+++ src/main/java/Util/PathwayFinder.java
@@ -1,26 +1,33 @@
 package Util;
 
+import DataTypes.PreconditionsException;
+import InputKnowledge.KnowledgeBase;
 import Miner.SabioMiner;
 import Parser.SBMLParser;
+import Parser.TSVParser;
 import org.sbml.jsbml.SBase;
 
 import javax.xml.stream.XMLStreamException;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class PathwayFinder {
 
     public static final String username = System.getProperty("user.name");
     public static final String projectFolder = "/home/"+username+"/Dropbox/Tesisti/software";
-    public static final String testFolder = projectFolder + "/test-cases/pathways";
+    public static final String testFolder = projectFolder + "/test-cases";
 
     public static void main(String args[]) throws IOException, XMLStreamException {
         PathwayFinder finder = new PathwayFinder();
 //        finder.findKineticInfoPathways(testFolder, 15, 2);
-        finder.findProteinPathways(testFolder, 2, 5);
+//        finder.findProteinPathways(testFolder, 2, 5);
 //        finder.findPathwaysWithFewReactions(testFolder, 5);
+        finder.cataloguePathways(testFolder);
     }
 
     public void findKineticInfoPathways(String folder, int maxNumReactions, int minNumProteins) throws IOException, XMLStreamException {
@@ -115,5 +122,102 @@ public class PathwayFinder {
         }
     }
 
+    public void cataloguePathways(String folder) throws IOException, XMLStreamException {
+        File folderFile = new File(folder);
+        SBMLParser sbmlParser = new SBMLParser(true);
+        TSVParser tsvParser = new TSVParser();
+        SabioMiner sabioMiner = new SabioMiner();
 
+        class PathwayInfo {
+            String pathwayName;
+            String pathwayId;
+            int numberOfSpecies;
+            int numberOfProteins;
+            int numberOfConstrainedProteins;
+            int numberOfReactions;
+            int numberOfConstrainedReactions;
+
+            PathwayInfo(String pathwayName, String pathwayId, int numberOfSpecies, int numberOfProteins, int numberOfConstrainedProteins, int numberOfReactions, int numberOfConstrainedReactions) {
+                this.pathwayName = pathwayName;
+                this.pathwayId = pathwayId;
+                this.numberOfSpecies = numberOfSpecies;
+                this.numberOfProteins = numberOfProteins;
+                this.numberOfConstrainedProteins = numberOfConstrainedProteins;
+                this.numberOfReactions = numberOfReactions;
+                this.numberOfConstrainedReactions = numberOfConstrainedReactions;
+            }
+
+            String getInfo() {
+                String info = "Pathway id: " + pathwayId + "\n" +
+                        "Number of species: " + numberOfSpecies + "\n" +
+                        "Number of proteins: " + numberOfProteins + "\n" +
+                        "Number of constrained proteins: " + numberOfConstrainedProteins + "\n" +
+                        "Number of reactions: " + numberOfReactions + "\n" +
+                        "Number of constrained reactions: " +
+                        ((numberOfConstrainedReactions == -1)? "COULD NOT GATHER INFO FROM SABIO" : String.valueOf(numberOfConstrainedReactions));
+                return info;
+            }
+        }
+
+        List<PathwayInfo> pathwayInfos = new ArrayList<>();
+
+        for (File pathway: folderFile.listFiles()) {
+            if (!pathway.isDirectory()) continue;
+            String pathwayName = pathway.getName();
+            System.out.print("Catalogue of " + pathwayName + "... ");
+            String pathwayFolderPath = folderFile + "/" + pathwayName;
+            String pathwayPath = pathwayFolderPath + "/in/pathway.sbml";
+            SBase tree = sbmlParser.getContent(pathwayPath);
+            String pathwayId = tree.getModel().getId();
+            int numberOfSpecies = tree.getModel().getListOfSpecies().size();
+            int numberOfReactions = tree.getModel().getListOfReactions().size();
+            int numberOfProteins = sbmlParser.getProteins(pathwayPath).size();
+
+            String abundancePath = pathwayFolderPath+ "/in/abundances.tsv";
+            int numberOfConstrainedProteins = tsvParser.getContent(abundancePath).size();
+
+            Set<String> reactionIds = tree.getModel().getListOfReactions().stream()
+                    .map(reaction -> reaction.getId())
+                    .collect(Collectors.toSet());
+
+            int numberOfConstrainedReactions;
+            try {
+                KnowledgeBase sabio = sabioMiner.mine(reactionIds);
+
+                numberOfConstrainedReactions = (int)sabio.getLinkContainsSet().stream()
+                        .map(link -> link.getKnowledgeAtom())
+                        .count();
+                sabio = null;
+
+            } catch (PreconditionsException exc) {
+                numberOfConstrainedReactions = -1;
+            }
+
+            PathwayInfo pathwayInfo = new PathwayInfo(pathwayName, pathwayId,
+                    numberOfSpecies, numberOfProteins, numberOfConstrainedProteins,
+                    numberOfReactions, numberOfConstrainedReactions);
+
+            pathwayInfos.add(pathwayInfo);
+            FileWriter fileWriter = new FileWriter(pathwayFolderPath + "/info.txt");
+
+            fileWriter.write(pathwayInfo.getInfo());
+            fileWriter.flush();
+            fileWriter.close();
+            System.out.print("done.\n");
+        }
+
+        System.out.print("Writing summary info... ");
+        FileWriter fileWriter = new FileWriter(folderFile + "/info.txt");
+
+        String info = "";
+        for (PathwayInfo pathwayInfo: pathwayInfos) {
+            info += pathwayInfo.pathwayName + "\n";
+            info += "\t" + pathwayInfo.getInfo().replace("\n", "\n\t") + "\n";
+        }
+
+        fileWriter.write(info);
+        fileWriter.flush();
+        fileWriter.close();
+        System.out.print("done.\n");
+    }
 }
